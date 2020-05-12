af87ccb02963dfd2a9997134d02020542213bcfe
Derek Thurn <derek@fake.email>
2020-05-12 10:25:17 -0700

    license header

--- Assets/Magewatch/Components/Creature.cs
+++ Assets/Magewatch/Components/Creature.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2020 © Derek Thurn
+﻿// Copyright © 2020-present Derek Thurn
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
