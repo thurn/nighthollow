2b220b5e21ab540db67256746f929632eac9afd3
Derek Thurn <derek@fake.email>
2020-08-11 07:52:59 -0700

    rm ludiq

--- Assets/Nighthollow/Data/CreatureData.cs
+++ Assets/Nighthollow/Data/CreatureData.cs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using Bolt;
 using Nighthollow.Components;
 using Nighthollow.Events;
 using UnityEngine;
