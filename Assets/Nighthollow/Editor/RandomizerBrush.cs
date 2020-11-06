37fedd0b55364a8df5416dcf430b0b4408d86324
Derek Thurn <derek@fake.email>
2020-11-06 08:10:41 -0800

    world map

---
+++ Assets/Nighthollow/Editor/RandomizerBrush.cs
@@ -0,0 +1,46 @@
+// Copyright © 2020-present Derek Thurn
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//    https://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Linq;
+using UnityEditor;
+using UnityEditor.Tilemaps;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace Nighthollow.Editor
+{
+  [CustomGridBrush(false, false, false, "Random Brush")]
+  [CreateAssetMenu(menuName = "2D/Randomizer Brush %#i")]
+  public sealed class RandomizerBrush : RandomBrush
+  {
+    public TileBase[] InputTiles;
+  }
+
+  [CustomEditor(typeof(RandomizerBrush))]
+  public sealed class RandomizerBrushEditor : RandomBrushEditor
+  {
+    public override void OnInspectorGUI()
+    {
+      base.OnInspectorGUI();
+      GUILayout.Space(24f);
+      if (GUILayout.Button("Import"))
+      {
+        var targetBrush = (RandomizerBrush) target;
+        targetBrush.randomTileSets = targetBrush.InputTiles
+          .Select(input => new RandomBrush.RandomTileSet {randomTiles = new[] {input}})
+          .ToArray();
+      }
+    }
+  }
+}
