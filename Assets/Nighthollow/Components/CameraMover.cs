6563a3fca78a9a2efee88f1a42b7e8d72bb28316
Derek Thurn <derek@fake.email>
2020-11-06 09:31:47 -0800

    camera movement

---
+++ Assets/Nighthollow/Components/CameraMover.cs
@@ -0,0 +1,80 @@
+// Copyright © 2020-present Derek Thurn
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//    https://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Nighthollow.Utils;
+using UnityEngine;
+
+#nullable enable
+
+namespace Nighthollow.Components
+{
+  public sealed class CameraMover : MonoBehaviour
+  {
+#pragma warning disable 0649
+    [SerializeField] Camera? _camera;
+    [SerializeField] float _keyboardMovementSpeed;
+    [SerializeField] float _scrollWheelZoomSpeed;
+    [SerializeField] float _keyboardZoomSpeed;
+    [SerializeField] float _minimumCameraSize;
+    [SerializeField] float _maximumCameraSize;
+    [SerializeField] float _maxXZoomedIn;
+    [SerializeField] float _maxXZoomedOut;
+    [SerializeField] float _maxYZoomedIn;
+    [SerializeField] float _maxYZoomedOut;
+    [Header("State")] [SerializeField] float _zoomDelta;
+#pragma warning restore 0649
+
+    int ZoomDirection()
+    {
+      var zoomIn = Input.GetKey(KeyCode.Z);
+      var zoomOut = Input.GetKey(KeyCode.X);
+
+      if (zoomIn && zoomOut)
+      {
+        return 0;
+      }
+      else if (!zoomIn && zoomOut)
+      {
+        return 1;
+      }
+      else if (zoomIn && !zoomOut)
+      {
+        return -1;
+      }
+      else
+      {
+        return 0;
+      }
+    }
+
+    void Update()
+    {
+      transform.Translate(
+        new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) *
+        (_keyboardMovementSpeed * Time.deltaTime), Space.Self);
+
+      _zoomDelta += Input.GetAxis("Mouse ScrollWheel") * Time.deltaTime * _scrollWheelZoomSpeed;
+      _zoomDelta += ZoomDirection() * Time.deltaTime * _keyboardZoomSpeed;
+      Errors.CheckNotNull(_camera).orthographicSize = Mathf.Lerp(
+        _minimumCameraSize, _maximumCameraSize, Mathf.Clamp01(_zoomDelta));
+
+      var maxX = Mathf.Lerp(_maxXZoomedIn, _maxXZoomedOut, _zoomDelta);
+      var maxY = Mathf.Lerp(_maxYZoomedIn, _maxYZoomedOut, _zoomDelta);
+      transform.position = new Vector3(
+        Mathf.Clamp(transform.position.x, -maxX, maxX),
+        Mathf.Clamp(transform.position.y, -maxY, maxY),
+        transform.position.z);
+    }
+  }
+}
