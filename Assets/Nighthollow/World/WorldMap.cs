d6c58c9340068abeffac078cbad964753f3b166a
Derek Thurn <derek@fake.email>
2020-11-06 22:43:12 -0800

    hex group selection

--- Assets/Nighthollow/World/WorldMap.cs
+++ Assets/Nighthollow/World/WorldMap.cs
@@ -14,6 +14,7 @@
 
 #nullable enable
 
+using System.Collections.Generic;
 using Nighthollow.Utils;
 using UnityEngine;
 using UnityEngine.Tilemaps;
@@ -22,74 +23,167 @@ namespace Nighthollow.World
 {
   public sealed class WorldMap : MonoBehaviour
   {
-    [SerializeField] Tilemap _overlayTilemap = null!;
-    public Tilemap OverlayTilemap => _overlayTilemap;
-
-    [SerializeField] Tilemap _tilemap = null!;
-    public Tilemap Tilemap => _tilemap;
-
-    [SerializeField] TilemapRenderer _tilemapRenderer = null!;
-    public TilemapRenderer TilemapRenderer => _tilemapRenderer;
-
     [SerializeField] Tile _bottomLeftSelection = null!;
     [SerializeField] Tile _bottomRightSelection = null!;
     [SerializeField] Tile _rightSelection = null!;
     [SerializeField] Tile _leftSelection = null!;
+    [SerializeField] Tilemap _overlayTilemap = null!;
 
-    public bool IsChild { get; set; }
+    const int BottomLeftZ = 1;
+    const int BottomRightZ = 2;
+    const int RightZ = 3;
+    const int LeftZ = 4;
+    readonly Dictionary<int, Tilemap> _children = new Dictionary<int, Tilemap>();
 
     void Start()
     {
-      _tilemap = GetComponent<Tilemap>();
-      _tilemapRenderer = GetComponent<TilemapRenderer>();
+      var map = ComponentUtils.GetComponent<Tilemap>(gameObject);
 
-      var color = new Color(0.776f, 0.157f, 0.157f);
-      _bottomLeftSelection.color = color;
-      _bottomRightSelection.color = color;
-      _rightSelection.color = color;
-      _leftSelection.color = color;
-
-      if (!IsChild)
+      // Unity is supposed to be able to handle overlapping by y coordinate correctly in a single chunked tilemap, but
+      // it's currently very buggy. So I just make each row in to a separate tilemap.
+      for (var yCoordinate = -30; yCoordinate <= 30; ++yCoordinate)
       {
-        // Unity is supposed to be able to handle overlapping by y coordinate correctly in a single tilemap, but it's
-        // currently very buggy. So I just make each row in to a separate tilemap.
-        for (var yCoordinate = -30; yCoordinate <= 30; ++yCoordinate)
+        var childMap = Instantiate(this, transform.parent);
+        Destroy(childMap.GetComponent<WorldMap>());
+        childMap.name = $"Row {yCoordinate}";
+        var tilemap = ComponentUtils.GetComponent<Tilemap>(childMap);
+        tilemap.ClearAllTiles();
+        ComponentUtils.GetComponent<TilemapRenderer>(childMap).sortingOrder = -yCoordinate;
+
+        for (var xCoordinate = -30; xCoordinate <= 30; ++xCoordinate)
         {
-          var childMap = ComponentUtils.GetComponent<WorldMap>(Instantiate(gameObject, transform.parent));
-          childMap.name = $"Row {yCoordinate}";
-          childMap.IsChild = true;
-          childMap.TilemapRenderer.sortingOrder = -yCoordinate;
-          childMap.Tilemap.ClearAllTiles();
-
-          for (var xCoordinate = -30; xCoordinate <= 30; ++xCoordinate)
-          {
-            childMap.Tilemap.SetTile(new Vector3Int(xCoordinate, yCoordinate, 0),
-              _tilemap.GetTile(new Vector3Int(xCoordinate, yCoordinate, 0)));
-          }
-
-          if (yCoordinate == 8)
-          {
-            childMap.Tilemap.SetTile(new Vector3Int(-12, 8, 1), Instantiate(_bottomRightSelection));
-            childMap.Tilemap.SetTile(new Vector3Int(-11, 8, 2), Instantiate(_bottomLeftSelection));
-            childMap.Tilemap.SetTile(new Vector3Int(-11, 8, 3), Instantiate(_bottomRightSelection));
-            childMap.Tilemap.SetTile(new Vector3Int(-10, 8, 3), Instantiate(_bottomLeftSelection));
-          }
-
-          if (yCoordinate == 7)
-          {
-            _overlayTilemap.SetTile(new Vector3Int(-12, 7, 2), _bottomRightSelection);
-            _overlayTilemap.SetTile(new Vector3Int(-12, 7, 3), _bottomLeftSelection);
-            _overlayTilemap.SetTile(new Vector3Int(-12, 7, 4), _leftSelection);
-
-            _overlayTilemap.SetTile(new Vector3Int(-11, 7, 2), _bottomRightSelection);
-            _overlayTilemap.SetTile(new Vector3Int(-11, 7, 3), _bottomLeftSelection);
-            _overlayTilemap.SetTile(new Vector3Int(-11, 7, 4), _rightSelection);
-
-          }
+          tilemap.SetTile(new Vector3Int(xCoordinate, yCoordinate, 0),
+            map.GetTile(new Vector3Int(xCoordinate, yCoordinate, 0)));
         }
 
-        Destroy(gameObject);
+        _children[yCoordinate] = tilemap;
+      }
+
+      map.ClearAllTiles();
+      Destroy(GetComponent<TilemapRenderer>());
+      Destroy(GetComponent<Tilemap>());
+
+      OutlineHexes(new Color(0.776f, 0.157f, 0.157f), new HashSet<Vector2Int>
+      {
+        new Vector2Int(-12, 7),
+        new Vector2Int(-11, 7),
+        new Vector2Int(-10, 7),
+        new Vector2Int(-12, 6),
+        new Vector2Int(-11, 6),
+      });
+    }
+
+    public void OutlineHexes(Color color, ISet<Vector2Int> hexes)
+    {
+      var bottomLeftSelection = Instantiate(_bottomLeftSelection);
+      var bottomRightSelection = Instantiate(_bottomRightSelection);
+      var rightSelection = Instantiate(_rightSelection);
+      var leftSelection = Instantiate(_leftSelection);
+      bottomLeftSelection.color = color;
+      bottomRightSelection.color = color;
+      rightSelection.color = color;
+      leftSelection.color = color;
+
+      foreach (var hex in hexes)
+      {
+        var upLeft = HexUtils.GetInDirection(hex, HexUtils.Direction.UpLeft);
+        if (!hexes.Contains(upLeft))
+        {
+          ApplySelection(upLeft, bottomRightSelection, BottomRightZ, toOverlay: false);
+        }
+
+        var upRight = HexUtils.GetInDirection(hex, HexUtils.Direction.UpRight);
+        if (!hexes.Contains(upRight))
+        {
+          ApplySelection(upRight, bottomLeftSelection, BottomLeftZ, toOverlay: false);
+        }
+
+        if (!hexes.Contains(HexUtils.GetInDirection(hex, HexUtils.Direction.Left)))
+        {
+          ApplySelection(hex, leftSelection, LeftZ, toOverlay: true);
+        }
+
+        if (!hexes.Contains(HexUtils.GetInDirection(hex, HexUtils.Direction.Right)))
+        {
+          ApplySelection(hex, rightSelection, RightZ, toOverlay: true);
+        }
+
+        if (!hexes.Contains(HexUtils.GetInDirection(hex, HexUtils.Direction.DownLeft)))
+        {
+          ApplySelection(hex, bottomLeftSelection, BottomLeftZ, toOverlay: true);
+        }
+
+        if (!hexes.Contains(HexUtils.GetInDirection(hex, HexUtils.Direction.DownRight)))
+        {
+          ApplySelection(hex, bottomRightSelection, BottomRightZ, toOverlay: true);
+        }
       }
     }
+
+    void ApplySelection(Vector2Int hex, Tile tile, int zPosition, bool toOverlay)
+    {
+      if (toOverlay)
+      {
+        _overlayTilemap.SetTile(new Vector3Int(hex.x, hex.y, zPosition), tile);
+      }
+      else
+      {
+        _children[hex.y].SetTile(new Vector3Int(hex.x, hex.y, zPosition), tile);
+      }
+    }
+
+    // void Start()
+    // {
+    //   _tilemap = GetComponent<Tilemap>();
+    //   _tilemapRenderer = GetComponent<TilemapRenderer>();
+    //
+    //   var color = new Color(0.776f, 0.157f, 0.157f);
+    //   _bottomLeftSelection.color = color;
+    //   _bottomRightSelection.color = color;
+    //   _rightSelection.color = color;
+    //   _leftSelection.color = color;
+    //
+    //   if (!IsChild)
+    //   {
+    //     // Unity is supposed to be able to handle overlapping by y coordinate correctly in a single tilemap, but it's
+    //     // currently very buggy. So I just make each row in to a separate tilemap.
+    //     for (var yCoordinate = -30; yCoordinate <= 30; ++yCoordinate)
+    //     {
+    //       var childMap = ComponentUtils.GetComponent<WorldMap>(Instantiate(gameObject, transform.parent));
+    //       childMap.name = $"Row {yCoordinate}";
+    //       childMap.IsChild = true;
+    //       childMap.TilemapRenderer.sortingOrder = -yCoordinate;
+    //       childMap.Tilemap.ClearAllTiles();
+    //
+    //       for (var xCoordinate = -30; xCoordinate <= 30; ++xCoordinate)
+    //       {
+    //         childMap.Tilemap.SetTile(new Vector3Int(xCoordinate, yCoordinate, 0),
+    //           _tilemap.GetTile(new Vector3Int(xCoordinate, yCoordinate, 0)));
+    //       }
+    //
+    //       if (yCoordinate == 8)
+    //       {
+    //         childMap.Tilemap.SetTile(new Vector3Int(-12, 8, 1), Instantiate(_bottomRightSelection));
+    //         childMap.Tilemap.SetTile(new Vector3Int(-11, 8, 2), Instantiate(_bottomLeftSelection));
+    //         childMap.Tilemap.SetTile(new Vector3Int(-11, 8, 3), Instantiate(_bottomRightSelection));
+    //         childMap.Tilemap.SetTile(new Vector3Int(-10, 8, 3), Instantiate(_bottomLeftSelection));
+    //       }
+    //
+    //       if (yCoordinate == 7)
+    //       {
+    //         _overlayTilemap.SetTile(new Vector3Int(-12, 7, 2), _bottomRightSelection);
+    //         _overlayTilemap.SetTile(new Vector3Int(-12, 7, 3), _bottomLeftSelection);
+    //         _overlayTilemap.SetTile(new Vector3Int(-12, 7, 4), _leftSelection);
+    //
+    //         _overlayTilemap.SetTile(new Vector3Int(-11, 7, 2), _bottomRightSelection);
+    //         _overlayTilemap.SetTile(new Vector3Int(-11, 7, 3), _bottomLeftSelection);
+    //         _overlayTilemap.SetTile(new Vector3Int(-11, 7, 4), _rightSelection);
+    //
+    //       }
+    //     }
+    //
+    //     Destroy(gameObject);
+    //   }
+    // }
   }
 }
